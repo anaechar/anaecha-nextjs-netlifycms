989a16ec91ee7f450e7144f00208b74a5de51f46
Anaecha Ratikarn <beforeifly@gmail.com>
2021-08-23 22:25:34 +0700

    cat page

--- layouts/CategoryLayout.js
+++ layouts/CategoryLayout.js
@@ -3,14 +3,14 @@ import NextLink from 'next/link';
 export default function CategoryLayout({ name, frontMatters }) {
     return (
         <section>
-            <h1>Category: {name}</h1>
+            <h1 className="py-2 text-5xl font-medium">Category: {name}</h1>
             {
                 frontMatters.map((frontMatter) => {
                     const { title, slug, publishedDate } = frontMatter;
                     return (
                         <div key={slug}>
-                            <h2><NextLink href={`/blog/${slug}`}>{title}</NextLink></h2>
-                            <div className="text-gray-500 dark:text-gray-500">{publishedDate}</div>
+                            <h2 className="text-base font-normal"><NextLink href={`/blog/${slug}`}>{title}</NextLink></h2>
+                            <div className="text-xs text-gray-500 dark:text-gray-500">{publishedDate}</div>
                         </div>
                     );
                 })
