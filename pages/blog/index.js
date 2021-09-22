94ef9d7144f21b4ae40fbb3f842c01ecb73d5357
Anaecha Ratikarn <beforeifly@gmail.com>
2021-09-22 18:15:51 +0700

    blog

---
+++ pages/blog/index.js
@@ -0,0 +1,30 @@
+import NextLink from 'next/link';
+import { getAllFilesFrontMatter } from '../../lib/mdx';
+import LayoutWrapper from '../../components/LayoutWrapper';
+import meta from '../../contents/meta.json';
+
+export async function getStaticProps() {
+  const allFilesFrontMatter = await getAllFilesFrontMatter('posts');
+  return { props: { allFilesFrontMatter }};
+};
+
+export default function HomePage({ allFilesFrontMatter }) {
+  return (
+    <LayoutWrapper meta={meta}>
+      <section>
+        <h2 className="text-3xl font-bold my-4">Latest Articles:</h2>
+        {
+          allFilesFrontMatter.map((fileFrontMatter) => {
+            const { title, slug, publishedDate, category } = fileFrontMatter;
+            return (
+              <div key={slug} className="my-3">
+                <h4 className="text-base font-normal"><NextLink href={`/blog/${slug}`}>{title}</NextLink></h4>
+                <div className="text-xs text-gray-500 dark:text-gray-500">{publishedDate}, {category}</div>
+              </div>
+            );
+          })
+        }
+      </section>
+    </LayoutWrapper>
+  );
+};
